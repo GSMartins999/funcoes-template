2e0d828d039fc460b71d59844a6a43a5b9111cdb
Giovanni Souza <GSMartins999>
2023-05-08 20:58:36 -0300

    funções

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,16 @@
 // Resolva o Exercício de Fixação!
+
+const dados = (nome1, nascimento, ano) => {
+    const nome = "Giovanni"
+    const anodeNascimento = Number(2006)
+    const anoAtual = Number(2023)
+
+    let maiordeIdade = anoAtual - anodeNascimento 
+    let idade = maiordeIdade >= 18
+    console.log(`Fulano é menor de idade: ${idade}`)
+    
+
+    return nome === nome1 && anodeNascimento === nascimento && anoAtual === ano 
+}
+
+console.log(dados("Giovanni", 2006, 2023))
